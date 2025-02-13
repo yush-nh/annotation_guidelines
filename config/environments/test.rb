9c2d8371b227cfcc7f8a872599f6b8abef4204d6
yush-nh <yushin.nihei@luxiar.com>
2025-02-13 16:08:32 +0900

    Trying to fix CI error4

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # To avoid NoMethodError in CI
+  config.action_cable.adapter = :test
 end
