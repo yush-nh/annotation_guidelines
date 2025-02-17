fa75ced6a27b3556a98c3d7353d0a763c97efa4e
yush-nh <yushin.nihei@luxiar.com>
2025-02-17 17:31:34 +0900

    Change rendering option of Commonmarker to align with easyMDE

--- app/helpers/annotation_guideline_helper.rb
+++ app/helpers/annotation_guideline_helper.rb
@@ -1,6 +1,11 @@
 module AnnotationGuidelineHelper
   def md2html(text)
-    html = Commonmarker.to_html(text, plugins: { syntax_highlighter: { theme: "InspiredGitHub" } })
+    html = Commonmarker.to_html(
+      text,
+      options: { render: { unsafe: true } },
+      plugins: { syntax_highlighter: { theme: "InspiredGitHub" } }
+    )
+
     raw(html)
   end
 end
