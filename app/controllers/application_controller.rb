a87963ab99815634b1e7248027e4a601aa19e13f
yush-nh <yushin.nihei@luxiar.com>
2025-02-17 15:32:39 +0900

    Delete unused helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  helper_method :current_user
-
   private
 
   # Set current_user until make login function.
