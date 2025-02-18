9dbb647408e9bc386573639bd76a5f746ed2e35c
yush-nh <yushin.nihei@luxiar.com>
2025-02-18 17:18:44 +0900

    Change DB table name from annotation_guidelines to notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_13_072025) do
-  create_table "annotation_guidelines", force: :cascade do |t|
+ActiveRecord::Schema[8.0].define(version: 2025_02_18_075402) do
+  create_table "notes", force: :cascade do |t|
     t.string "title", null: false
     t.text "body"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_annotation_guidelines_on_user_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,5 +32,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_13_072025) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "annotation_guidelines", "users"
+  add_foreign_key "notes", "users"
 end
