370daec53eb540f3a0d61888968d245b311d6926
yush-nh <yushin.nihei@luxiar.com>
2025-02-18 15:24:51 +0900

    Add explanation for why html_safe is needed in html_body method

--- app/models/annotation_guideline.rb
+++ app/models/annotation_guideline.rb
@@ -3,6 +3,9 @@ class AnnotationGuideline < ApplicationRecord
 
   validates :title, presence: :true, length: { maximum: 255 }
 
+  # Commonmarker ensures that potentially dangerous tags like <script> are already sanitized.
+  # However, Brakeman still raises a Cross-Site Scripting warning when the raw output of Commonmarker.to_html is used in views.
+  # To suppress the warning, explicitly mark the returned HTML as safe using .html_safe.
   def html_body
     Commonmarker.to_html(
       body,
