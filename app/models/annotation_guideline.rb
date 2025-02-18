e5ad5b5424638ce4143a16223bcb709c370f9117
yush-nh <yushin.nihei@luxiar.com>
2025-02-18 09:26:35 +0900

    Move markdown conversion from helper to model method

--- app/models/annotation_guideline.rb
+++ app/models/annotation_guideline.rb
@@ -2,4 +2,12 @@ class AnnotationGuideline < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: :true, length: { maximum: 255 }
+
+  def html_body
+    Commonmarker.to_html(
+      body,
+      options: { render: { unsafe: true } },
+      plugins: { syntax_highlighter: { theme: "InspiredGitHub" } }
+    )
+  end
 end
