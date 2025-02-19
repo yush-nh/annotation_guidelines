82432959e6aa3232ab7f6111a1b674f220aade2b
yush-nh <yushin.nihei@luxiar.com>
2025-02-19 15:23:05 +0900

    Delete temporary defined current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  private
-
-  # Set current_user until make login function.
-  def current_user
-    @current_user ||= User.first
-  end
 end
