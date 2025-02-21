617d9591b7929483ca1ee1cc3da1c0b31f0746ed
yush-nh <yushin.nihei@luxiar.com>
2025-02-21 13:57:36 +0900

    Add note read API

--- test/controllers/api/v1/notes_controller_test.rb
+++ test/controllers/api/v1/notes_controller_test.rb
@@ -6,6 +6,7 @@ class Api::V1::NotesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
     sign_in @user
+    @note = notes(:one)
   end
 
   test "POST_should_create_note" do
@@ -14,11 +15,25 @@ class Api::V1::NotesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "GET_should_show_note" do
+    get "/api/v1/notes/#{@note.uuid}"
+
+    json_response = JSON.parse(response.body)
+    assert_equal @note.title, json_response["title"]
+    assert_equal @note.body, json_response["body"]
+  end
+
   # Exceptions Test
 
   test "should_return_422_when_record_invalid" do
     post "/api/v1/notes", params: { title: "", body: "" }, as: :json
 
     assert_response 422
+  end
+
+  test "should_return_404_when_record_not_found" do
+     get "/api/v1/notes/123"
+
+     assert_response 404
   end
 end
